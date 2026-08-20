63640806429cc964f3b2faa7368612e9ce6f4d00
Aperat <D.Aperat@isida.by>
2026-08-20 14:41:39 +0300

    fix next.config

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,18 @@
+// next.config.ts
 import type { NextConfig } from 'next';
 
+const isDev = process.env.NODE_ENV === 'development';
+
 const nextConfig: NextConfig = {
-  output: 'export',
-  basePath: '/newsnapbild',
+  output: isDev ? undefined : 'export',
+  basePath: isDev ? '' : '/newsnapbild',
   images: {
     unoptimized: true,
   },
   reactStrictMode: true,
   trailingSlash: true,
   env: {
-    NEXT_PUBLIC_BASE_PATH: '/newsnapbild',
+    NEXT_PUBLIC_BASE_PATH: isDev ? '' : '/newsnapbild',
   },
 };
 
